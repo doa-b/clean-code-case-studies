716ae81e2464490a4d95de50501908b1b5694b7e
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2021-07-02 14:49:37 +0200

    first commit

---
+++ chapter15-junit-internals/src/main/java/org/doa/App.java
@@ -0,0 +1,13 @@
+package org.doa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
